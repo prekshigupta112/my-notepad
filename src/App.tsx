949a278be01b4b610b109c15cafe25ff1b09cdc2
Prekshi Gupta <prekshigupta123@gmail.com>
2023-11-30 21:34:09 +0530

    Integrated GET and POST APIs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+
+  const handleChange = (val: string) => {
+    setData(val);
+    console.log(val);
+  };
+
+  useEffect(() => {
+    fetchContent();
+    uploadContent();
+  }, []);
+
+  const fetchContent = () => {
+    axios
+      .get("http://localhost:3000/67")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const uploadContent = () => {
+    axios
+      .post("http://localhost:3000/67", {
+        data: "hehehe",
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      Hello World! This is my notepad.
+      <textarea
+        name="data"
+        value={data}
+        onChange={(e) => handleChange(e.target.value)}
+      />
     </div>
   );
 }
